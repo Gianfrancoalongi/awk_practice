6dfd17711480c73e82a86a1d4cf94e7597b09c29
gianfranco <gianfranco.alongi@gmail.com>
2014-02-02 21:01:53 +0100

    Added scenario 03, changing Field separators

--- test/run_tests.bash
+++ test/run_tests.bash
@@ -4,7 +4,7 @@ DONE="Verified - you are done"
 NOT_DONE="No - you are not done"
 
 main() {
-   for((x=1;x<=2;x++))
+   for((x=1;x<=3;x++))
    do
        if [[ ${x} -lt 10 ]]
        then
@@ -29,6 +29,10 @@ solution_for_scenario_01() {
 
 solution_for_scenario_02() {
     echo '$6 == "m"' > answer.awk
+}
+
+solution_for_scenario_03() {
+    echo 'BEGIN {FS=","} ; $6 == "m"' > answer.awk
 }
 
 test_that_verification_fails_for_scenario() {
